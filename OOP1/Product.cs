1db27bb25c9d29ba943dae6a7d3b473e3d10d275
example <dmkn01@hotmail.com>
2021-01-29 15:48:23 +0300

    Güncelleme

---
+++ OOP1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet
+    class Product 
+    {
+        public int Id { get; set; }//Primary Key
+        public int CategoryId { get; set; }//Foreign key
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        
+
+
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //void diye belirtilen methodlar (git ekle git güncelle) 
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "eklendi");
+        }
+        public void Update (Product product)
+        {
+            Console.WriteLine(product.ProductName+"güncellendi");
+        }
+        public int Topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+  
+     
+        //
+    }
+}
